565b7eb777ce0446cb4615348f7a049682ef980a
David Quiñones <daquinons@users.noreply.github.com>
2019-10-02 13:26:59 +0200

    Create Player

--- src/player.py
+++ src/player.py
@@ -1,2 +1,20 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player():
+    def __init__(self, name, current_room):
+        self.__name = name
+        self.__current_room = current_room
+
+    @property
+    def current_room(self):
+        return self.__current_room
+
+    @current_room.setter
+    def current_room(self, current_room):
+        self.__current_room = current_room
+
+    @property
+    def name(self):
+        return self.__name
